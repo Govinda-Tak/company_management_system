b5cd5010e914b63380f32104b895caa115c7e6a1
Govinda-Tak <govinda.tak@atmecs.com>
2024-07-22 13:19:26 +0530

    implementation of changes recommend by mentor.

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -37,7 +37,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString(exclude = {"project"})
+@ToString(exclude = {"projects"})
 public class Employee extends BaseModel {
 	@NotBlank(message = "Employee name required !!")
 	@Column(nullable = false)
@@ -69,7 +69,7 @@ public class Employee extends BaseModel {
 	
 	@ManyToMany
 	@JoinTable(name = "projects_employees",joinColumns = @JoinColumn(name="employee_id",nullable = false),inverseJoinColumns = @JoinColumn(name="project_id",nullable = false))
-	private List<Project> project;
+	private List<Project> projects;
 	
 	@Embedded
 	private Address address;
